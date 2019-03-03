cf997404fa3640f5a63b48aeecdb6e579c4b4b5d
Ming Duan <11203duan@gmail.com>
2019-03-03 19:16:52 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,37 @@
-# code here!
+class School
+
+def initialize(neww)
+  @name = neww
+  @roster = {}
+end
+
+def name
+  @name
+end
+
+def roster
+  @roster
+end
+
+def add_student(stu, grade)
+  if @roster[grade].nil?
+    arr = []
+    arr << stu
+    @roster[grade] = arr
+else
+  @roster[grade] << stu
+end
+end
+
+def grade(grad)
+  @roster[grad]
+end
+
+def sort
+  @roster.each do
+    |key, value| value.sort!
+  end
+end
+
+
+end
